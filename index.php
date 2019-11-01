5b48fd9ba728f9b4ed95cb46831a0e7c226c0762
Leon Chen <a781282000@gmail.com>
2019-11-01 20:49:09 +0800

    註冊登入頁面製作

---
+++ index.php
@@ -0,0 +1,28 @@
+<html lang="zh-hant">
+  <head>
+    <meta http-equiv="Content-Language" content="zh-tw">
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <title>留言板-登入</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+  </head>
+  <body class="background">
+    <div class="header">
+      <a href="index.php" class="header_word">登入</a>
+      <a href="register.php" class="header_word">註冊</a>
+    </div>  
+    <form action="login.php" method="post" class="form_card">
+      <p>登入：</p>
+      <div>
+        <input type="text" name="account" placeholder="請輸入帳號..." class="input_text">
+      </div>
+      <div>  
+        <input type="password" name="password" placeholder="請輸入密碼..." class="input_password">
+      </div>
+      <div>
+        <button type="submit" class="button">登入</button>
+      </div>  
+      <p>還不是會員</p>
+      <p><a href="register.php" class="member">加入會員</a> 
+    </form>   
+  </body>  
+</html>
---
+++ register.php
@@ -0,0 +1,32 @@
+<html lang="zh-hant">
+  <head>
+    <meta http-equiv="Content-Language" content="zh-tw">
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <title>留言板-註冊</title>
+    <link rel="stylesheet" type="text/css" href="css/register.css">
+  </head>
+  <body class="background">
+    <div class="header">
+      <a href="index.php" class="header_word">登入</a>
+      <a href="register.php" class="header_word">註冊</a>
+    </div>  
+    <form action="login.php" method="post" class="form_card">
+      <p>會員註冊：</p>
+      <div>
+        <input type="text" name="account" placeholder="請輸入名稱..." class="input_text">
+      </div>
+      <div>  
+        <input type="password" name="password" placeholder="請輸入帳號..." class="input_password">
+      </div>
+      <div>  
+        <input type="password" name="password" placeholder="請輸入密碼..." class="input_password">
+      </div>
+      <div>  
+        <input type="password" name="password" placeholder="再一次輸入密碼..." class="input_password">
+      </div>
+      <div>
+        <button type="submit" class="button">註冊</button>
+      </div>   
+    </form>   
+  </body>  
+</html>
