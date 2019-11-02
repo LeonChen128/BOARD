1b1a8264806ae15cdf55b86c3503c9b125b1c260
Leon Chen <a781282000@gmail.com>
2019-11-02 11:52:51 +0800

    註冊畫面修改

--- register.php
+++ register.php
@@ -10,19 +10,19 @@
       <a href="index.php" class="header_word">登入</a>
       <a href="register.php" class="header_word">註冊</a>
     </div>  
-    <form action="login.php" method="post" class="form_card">
+    <form action="register2.php" method="post" class="form_card">
       <p>會員註冊：</p>
       <div>
-        <input type="text" name="account" placeholder="請輸入名稱..." class="input_text">
+        <input type="text" name="name" placeholder="請輸入名稱..." class="input_text">
       </div>
       <div>  
-        <input type="password" name="password" placeholder="請輸入帳號..." class="input_password">
+        <input type="text" name="account" placeholder="請輸入帳號..." class="input_password">
       </div>
       <div>  
         <input type="password" name="password" placeholder="請輸入密碼..." class="input_password">
       </div>
       <div>  
-        <input type="password" name="password" placeholder="再一次輸入密碼..." class="input_password">
+        <input type="password" name="repassword" placeholder="再一次輸入密碼..." class="input_password">
       </div>
       <div>
         <button type="submit" class="button">註冊</button>
